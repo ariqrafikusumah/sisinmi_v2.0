633594897e495b20ee0c22a8d50dbe1f79134ab1
ilhamdwi <56429734+ilhamdwi@users.noreply.github.com>
2023-03-14 14:49:04 +0700

    melakukan proses tambah user -ilham

---
+++ src/function/proses_tambahuser.php
@@ -0,0 +1,25 @@
+<?php
+
+// Memanggil file koneksi.php
+require_once('../config/database.php');
+
+// Mengecek apakah form telah disubmit
+if (isset($_POST['email']) && isset($_POST['password']) && isset($_POST['role'])) {
+    // Mengambil data dari form
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $role = $_POST['role'];
+
+    // Membuat query SQL untuk menambahkan data user ke dalam tabel
+    $sql = "INSERT INTO user (email, password, role) VALUES ('$email', '$password', '$role')";
+    $result = mysqli_query($koneksi, $sql);
+
+    // Mengecek apakah data berhasil ditambahkan
+    if ($result) {
+        echo "Data berhasil ditambahkan";
+    } else {
+        echo "Error: " . mysqli_error($koneksi);
+    }
+}
+
+?>
--- src/pages/admin/components/Form_User.php
+++ src/pages/admin/components/Form_User.php
@@ -1,4 +1,4 @@
-<form action="" method="post">
+<form action="../../function/proses_tambahuser.php" method="post">
     <div>
         <div class="mb-3">
             <label for="email" class="form-label">Email</label>
@@ -9,17 +9,18 @@
             <input id="password" name="password" class="form-control" type="password" placeholder="Enter Password">
         </div>
         <div class="mb-3">
-            <label for="role" class="form-label">Password</label>
-            <select name="role" id="role" name="role" class="form-select" aria-label="Default select example">
-                <option selected>- Select Role -</option>
+            <label for="role" class="form-label">Role</label>
+            <select id="role" name="role" class="form-select" aria-label="Default select example">
+                <option selected disabled>- Select Role -</option>
                 <option value="admin">admin</option>
                 <option value="dosen">dosen</option>
                 <option value="baak">baak</option>
+                <option value="kaprodi">kaprodi</option>
             </select>
         </div>
         <div class="mb-3">
             <button type="reset" class="btn btn-danger bg-red-500 hover:bg-red-600">Reset</button>
-            <button type="submit" class="btn btn-primary bg-blue-500 hover:bg-blue-600">Reset</button>
+            <button type="submit" class="btn btn-primary bg-blue-500 hover:bg-blue-600">Save</button>
         </div>
     </div>
 </form>
